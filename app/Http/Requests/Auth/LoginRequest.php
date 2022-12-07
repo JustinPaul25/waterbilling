5222cd42663db58d9cd1a9185768e3321878c1bb
Mark Gabinete <justinpaul1025@gmail.com>
2022-12-07 19:48:23 +0800

    update login

--- app/Http/Requests/Auth/LoginRequest.php
+++ app/Http/Requests/Auth/LoginRequest.php
@@ -11,6 +11,21 @@ use Illuminate\Validation\ValidationException;
 
 class LoginRequest extends FormRequest
 {
+    protected $loginField;
+    protected $loginValue;
+    /**
+     * Prepare the data for validation.
+     *
+     * @return void
+     */
+    protected function prepareForValidation()
+    {
+        $this->loginField = filter_var($this->input('login'),
+        FILTER_VALIDATE_EMAIL) ? 'email' : 'username';
+        $this->loginValue = $this->input('login');
+        $this->merge([$this->loginField => $this->loginValue]);
+    }
+
     /**
      * Determine if the user is authorized to make this request.
      *
@@ -29,8 +44,11 @@ class LoginRequest extends FormRequest
     public function rules()
     {
         return [
-            'email' => ['required', 'string', 'email'],
-            'password' => ['required', 'string'],
+            'email' =>
+                'required_without:username|string|email|exists:users,email',
+            'username' =>
+                'required_without:email|string|exists:users,username',
+            'password' => 'required|string',
         ];
     }
 
@@ -44,15 +62,16 @@ class LoginRequest extends FormRequest
     public function authenticate()
     {
         $this->ensureIsNotRateLimited();
-
-        if (! Auth::attempt($this->only('email', 'password'), $this->boolean('remember'))) {
+        if (!Auth::attempt(
+                $this->only($this->loginField, 'password'), 
+                $this->boolean('remember')
+            )) 
+        {
             RateLimiter::hit($this->throttleKey());
-
             throw ValidationException::withMessages([
-                'email' => trans('auth.failed'),
+            'login' => __('auth.failed')
             ]);
         }
-
         RateLimiter::clear($this->throttleKey());
     }
 
