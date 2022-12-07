8201605ae60e84cd7f1cddda95e5bcb2eb3ee2fe
Mark Gabinete <justinpaul1025@gmail.com>
2022-12-07 18:06:39 +0800

    update

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,8 +15,15 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->string('name')->nullable();
+            $table->string('first_name')->nullable();
+            $table->string('middle_name')->nullable();
+            $table->string('last_name')->nullable();
+            $table->string('contact_no')->nullable();
+            $table->string('address')->nullable();
+            $table->boolean('status')->dafault(true);
+            $table->string('email')->unique()->nullable();
+            $table->string('username')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
